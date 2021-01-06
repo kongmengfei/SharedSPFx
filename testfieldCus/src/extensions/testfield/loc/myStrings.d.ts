5917d044c58f89c6b632a6b9d188852d64de2bf0
kongmengfei <57654821+kongmengfei@users.noreply.github.com>
2021-01-06 09:29:52 +0800

    ef
    fe

---
+++ testfieldCus/src/extensions/testfield/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface ITestfieldFieldCustomizerStrings {
+  Title: string;
+}
+
+declare module 'TestfieldFieldCustomizerStrings' {
+  const strings: ITestfieldFieldCustomizerStrings;
+  export = strings;
+}
