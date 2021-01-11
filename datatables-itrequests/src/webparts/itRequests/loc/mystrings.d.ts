848ab789b4496b9edb2b1c880c08e0bda831c51a
kongmengfei <57654821+kongmengfei@users.noreply.github.com>
2021-01-11 15:18:22 +0800

    21-1-11
    15:18

---
+++ datatables-itrequests/src/webparts/itRequests/loc/mystrings.d.ts
@@ -0,0 +1,11 @@
+declare interface IItRequestsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  ListNameFieldLabel: string;
+}
+
+declare module 'ItRequestsWebPartStrings' {
+  const strings: IItRequestsWebPartStrings;
+  export = strings;
+}
+
